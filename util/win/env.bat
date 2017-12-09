0da0b2b9ee72b9dad7248993ca49703ca59f4dd1
Gres <gres@gres.biz>
2017-12-09 18:16:36 +0300

    Updated version. Win not ready.

--- util/win/env.bat
+++ util/win/env.bat
@@ -1,21 +1,21 @@
 @echo off
 
-if "%ARCH%" == "" set ARCH=x86
+if "%ARCH%" == "" set ARCH=x64
 
 if /i %ARCH% == x86       goto x86
 if /i %ARCH% == x64       goto x64
 goto end
 
 :x64
-call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-set PATH=c:\Qt\5.9\msvc2015_64\bin\;%PATH%
-set QT_DIR=c:\Qt\5.9\msvc2015_64\
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+set PATH=c:\Qt\5.10\msvc2017_64\bin\;%PATH%
+set QT_DIR=c:\Qt\5.10\msvc2017_64\
 goto end
 
 :x86
-call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-set PATH=c:\Qt\5.9\msvc2015\bin\;%PATH%
-set QT_DIR=c:\Qt\5.9\msvc2015\
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+set PATH=c:\Qt\5.10\msvc2017\bin\;%PATH%
+set QT_DIR=c:\Qt\5.10\msvc2017\
 goto end
 
 :end
