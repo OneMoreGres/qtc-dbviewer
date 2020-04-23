504c158b35ecaf05973357c340b5a3b331d1d0be
Gres <gres@gres.biz>
2020-04-23 19:45:33 +0300

    Update versions

--- util/ci/config.py
+++ util/ci/config.py
@@ -1,7 +1,7 @@
 from os import getenv, path
 
-qt_version = "5.14.0"
-qtc_version = "4.11.0"
+qt_version = "5.14.2"
+qtc_version = "4.12.0"
 os_name = getenv('OS', 'linux')
 qt_modules = ['qtbase', 'qttools', 'icu']
 plugin_name = 'QtcDbViewer'
